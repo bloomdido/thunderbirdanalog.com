fa0b6a611676580362590da10b7586136aebafba
Unknown <dave@hapnyn.com>
2018-05-02 16:40:50 -0700

    Styled text on the body.

--- src/bundles/components/Content/Body/index.js
+++ src/bundles/components/Content/Body/index.js
@@ -1,15 +1,24 @@
 import * as React from 'react'
 import {
   Box,
+  Text,
 } from 'bundles/ui/core'
 
 const Body = () => {
   return (
-     <Box
-      backgroundColor="rgba(210,180,140,.5)"
-      height={500}
-    >
-      BODY
+     <Box display='flex' backgroundColor="rgba(210,180,140,.5)">
+      <Box padding={40}>
+        <Text component='h1'>About Thunderbird</Text>
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </Text>
+      </Box>
+      <Box padding={40}>
+        <Text component='h1'>Recording Equipment</Text>
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </Text>
+      </Box>
     </Box>
   )
 }
