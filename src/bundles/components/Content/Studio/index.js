a2babb69eab5ca0ab0168d5a400909d95f8a840a
Unknown <dave@hapnyn.com>
2018-05-07 14:51:57 -0700

    Added images to Studio page

--- src/bundles/components/Content/Studio/index.js
+++ src/bundles/components/Content/Studio/index.js
@@ -1,16 +1,31 @@
 import * as React from 'react'
-import {
-  Box,
-  Text,
-} from 'bundles/ui/core'
+import Carousel from 'bundles/components/Content/Carousel'
+import image1 from 'src/images/20160411_182652.jpg'
+import image2 from 'src/images/20171024_200044.jpg'
+import image3 from 'src/images/20171024_200054.jpg'
+import image4 from 'src/images/20171024_200353.jpg'
+import image5 from 'src/images/20171024_200357.jpg'
+import image6 from 'src/images/20171024_195649.jpg'
 
 const Studio = () => {
+  const maxColumns = 3
+  const maxRows = 2
+
+  const allPictures = [
+    image1,
+    image2,
+    image3,
+    image4,
+    image5,
+    image6,
+  ]
+
   return (
-     <Box display='flex' backgroundColor='rgba(199,204,178,.6)'>
-      <Box padding={40} width='50%'>
-        <Text component='h1' paddingBottom={30}>Studio</Text>
-      </Box>
-    </Box>
+    <Carousel
+      allPictures={allPictures}
+      maxRows={maxRows}
+      maxColumns={maxColumns}
+    />
   )
 }
 
