34848fbd4602d1998d4da003781917330819bef1
Unknown <dave@hapnyn.com>
2018-05-07 11:26:16 -0700

    Center image thumbnails

--- src/bundles/components/Content/ImagePreview/index.js
+++ src/bundles/components/Content/ImagePreview/index.js
@@ -7,7 +7,7 @@ import {
 const ImagePreview = ({image}) => {
   return (
     <Box display="flex" justifyContent="center">
-      <Image width="370px" height="208px">{image}</Image>
+      <Image width="370px" height="208px" paddingLeft={10} paddingRight={10} paddingTop={5} paddingBottom={5}>{image}</Image>
     </Box>
   )
 }
