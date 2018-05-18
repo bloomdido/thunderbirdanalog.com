f1aa4da6bc715bb310d5bdac38207e10d8813659
Dave Cornell <dave@hapnyn.com>
2018-05-18 12:48:21 -0700

    Added sample mobile menu

--- src/bundles/components/Content/Menubar/HamburgerMenu/index.js
+++ src/bundles/components/Content/Menubar/HamburgerMenu/index.js
@@ -1,8 +1,13 @@
 import * as React from 'react'
+import {Transition} from 'react-transition-group'
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 import {faBars} from '@fortawesome/fontawesome-free-solid'
 import {
+  Box,
   Button,
+  List,
+  ListItem,
+  Text,
 }
 from 'bundles/ui/core'
 
@@ -13,15 +18,42 @@ class HamburgerMenu extends React.PureComponent {
 
   render () {
     return (
-      <Button
-        onClick={this.handleClick}
-        backgroundColor="transparent"
-        color="white"
-        border="none"
-        focusOutline="none"
-      >
-        <FontAwesomeIcon icon={faBars} size="4x"/>
-      </Button>
+      <Box>
+        <Button
+          onClick={this.handleClick}
+          backgroundColor="transparent"
+          color="white"
+          border="none"
+          focusOutline="none"
+        >
+          <FontAwesomeIcon icon={faBars} size="4x"/>
+        </Button>
+        <Box
+          width="80%"
+          height="100%"
+          backgroundColor="rgba(0, 77, 0, 0.75)"
+          opacity={0.75}
+          position="fixed"
+          top={0}
+          right={0}
+          borderLeft="solid"
+        >
+          <Text
+            component='h1'
+            paddingTop={30}
+            paddingLeft={30}
+          >
+            menu item 1
+          </Text>
+          <Text
+            component='h1'
+            paddingTop={30}
+            paddingLeft={30}
+          >
+            menu item 2
+          </Text>
+        </Box>
+      </Box>
     )
   }
 }
